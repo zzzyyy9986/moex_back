73f6e644fc03aeec67be20ad935f1a5bb14174f8
zahar <zzzyyy589@gmail.com>
2023-12-10 09:14:21 +0300

    add front

---
+++ controllers/StocksController.ts
@@ -0,0 +1,65 @@
+import * as express from "express";
+import axios from "axios";
+import {StocksCommandService} from "../services/StocksCommandService";
+import {tickersKeyWords} from "../consts/tickers";
+import {forEach} from "lodash";
+
+
+//1) Новостная оценка
+// python /root/proton/sentiment.py  --ticker "SBER" --search_query "Сбербанк | Сбер"
+//1.1) Оценка по рынкам
+// python /root/proton/get_price_change.py --ticker "SBER" --period "week"
+//2)
+//3) Оценка новости
+// python /root/proton/sent_title.py --title "Сбер отчитался выше ожиданий рынка"
+
+
+export class StocksController {
+    // газпром|сбер|альфа|нефть
+    public static async getNewsByTicker(req: express.Request, res: express.Response){
+        const util = require('util');
+        const exec = util.promisify(require('child_process').exec);
+        let resp = []
+
+        const ticker = req.body.ticker
+
+        console.log(Object.keys(tickersKeyWords))
+
+
+        const newsResp = await axios.get(`https://mediametrics.ru/satellites/api/search/?ac=search&nolimit=1&q=${tickersKeyWords[ticker]}&p=1&c=ru&d=month&sort=tm&dbg=debug&callback=JSON`)
+
+         for(const item of newsResp.data.items) {
+                console.log(item.title)
+                const {stdout, stderr} = await exec(StocksCommandService.getRateByTitle(item.title))
+                console.log(stdout)
+                resp.push({
+                    id: item.id,
+                    title: item.title,
+                    url: item.url,
+                    value: stdout.replace("\n",""),
+                    source:item.url.split("/")[0],
+                    time:item.timestamp,
+                    ticker,
+                })
+        }
+        res.send({
+            data:resp
+        })
+    }
+    public static async getStocksList(req: express.Request, res: express.Response){
+        let  resp = []
+        const util = require('util');
+        const exec = util.promisify(require('child_process').exec);
+
+        for (const ticker of Object.keys(tickersKeyWords)) {
+            const { stdout, stderr } = await exec(StocksCommandService.getRateByKeyWord(ticker,tickersKeyWords[ticker]));
+             resp.push({
+                 ticker,
+                 value:stdout
+             })
+        }
+        await res.send({
+            data:resp
+        })
+    }
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,37 @@
 import * as express from "express";
+import {StocksController} from "./controllers/StocksController";
 require("dotenv").config();
 
 const app = express();
 const port = 8080;
 const pathToFront = __dirname + "/build/";
+var cors = require("cors");
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
 app.use(express.static(pathToFront));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
 
 
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.sendFile(pathToFront + "index.html");
     res.send('Express + TypeScript Server');
+});
+app.get('/getNewsByTicker', (req: express.Request, res: express.Response) => {
+    StocksController.getNewsByTicker(req, res)
+});
+app.post('/getNewsByTicker', (req: express.Request, res: express.Response) => {
+    StocksController.getNewsByTicker(req, res)
+});
+
+app.post('/getStocksList', (req: express.Request, res: express.Response) => {
+    StocksController.getStocksList(req, res)
 });
 
 app.listen(port, () => {
